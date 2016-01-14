592b011673f020f263245f8394d5b090bd969eb0
Brian Cullen <brian.cullen@gmail.com>
2016-01-14 20:49:03 +0000

    Added Threaded version of the code. Moved the python source to the python_src folder.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # URLScraper
 Simple program to count the number of links to pages in a website and rank them by the number of links. Created as part of an experiment with a class with a class when we were discussing ways in which google ranked pages.
 
-To run simply execute the provided python file with a Python3 interpreter. The program will ask for the base URL which should point to the home page for the website that you want to check. For example to check Google you would use http://www.google.com/ as the baseURL. 
+There are now two versions of the program, one threaded and one not. To run the program simply execute the provided python file (in the `python_src` folder) with a Python3 interpreter. Both of the programs will ask for the base URL which should point to the home page for the website that you want to check. For example to check Google you would use http://www.google.com/ as the baseURL.
 
 At the moment the program outputs a line for each page it checks (along with any errors) so for large sites there will be a lot of output. At the end a sorted set of tuples will be printed out showing how many links there were to each page in the website. The page with the largest number of links is shown at the bottom of the list.
 
@@ -14,3 +14,4 @@ Please be aware that if you run this program it will request every webpage from 
  * Can be used to demonstrate the differences between sets, arrays and tuples in Python.
  * Can be used to demonstrate regular expressions and searching text files (that is how the links are extracted).
  * Can be used to show how network resources can be accessed via Python.
+ * Can be used to show the advantages and issues involved with Threading.
---
+++ python_src/Threaded_Scraper.py
@@ -0,0 +1,102 @@
+import urllib.request
+import threading
+import time
+import re
+
+lock = threading.Lock()
+
+def checkURLs (urlsToCheck, urlsChecked, linkCount):
+	lock.acquire()
+	while len(urlsToCheck) > 0:
+		currentURL = urlsToCheck.pop()
+		urlsChecked.add(currentURL)
+		#print("Checking page (" + currentURL + ") -", threading.get_ident())
+		lock.release()
+
+		try:
+			response = urllib.request.urlopen(currentURL)
+
+			if response.status != 200 or not(response.headers["Content-Type"].lower().startswith("text/html")):
+				continue
+
+			the_page = response.read().decode("utf-8")
+
+			data = re.findall('<a [^>]*href="([^"]*)"[^>]*>', the_page)
+			for url in data:
+				if len(url) == 0 or url[0] == '#':
+					continue
+				elif url[0] == '/':
+					url = baseURL + url[1:]
+				elif not url.startswith(baseURL):
+					continue
+
+				lock.acquire()
+				if url not in urlsChecked:
+					urlsToCheck.add(url)
+
+				if url in linkCount:
+					linkCount[url] += 1
+				else:
+					linkCount[url] = 1
+				lock.release()
+				
+		except urllib.error.HTTPError as error:
+			print("** Error code", error.code, "for", currentURL)
+		except ValueError as error:
+			print("** Invalid URL:", currentURL)
+		finally:
+			lock.acquire()
+	lock.release()
+
+
+
+inputURL = input('Enter the base URL: ')
+if len(inputURL) == 0:
+    print("No URL provided.")
+    exit(1)
+
+if not inputURL.startswith('http'):
+    inputURL = 'http://' + inputURL
+
+if re.search('\\.(html?|php)$', inputURL) is not None:
+    index = inputURL.rfind('/')
+    baseURL = inputURL[:index+1]
+    print(baseURL)
+elif not inputURL.endswith('/'):
+    baseURL = inputURL + '/'
+else:
+    baseURL = inputURL
+
+urlsChecked = set()
+urlsToCheck = {inputURL}
+
+# Put inputURL to -1 so that it doesn't
+# count the fact that we are starting there
+# as a link.
+linkCount = {inputURL: -1}
+
+try:
+	threads = [threading.Thread(None, checkURLs, None, (urlsToCheck, urlsChecked, linkCount))]
+	threads[0].start()
+	time.sleep(1)
+
+	for index in range(5):
+		thread = threading.Thread(None, checkURLs, None, (urlsToCheck, urlsChecked, linkCount))
+		thread.start()
+		threads.append(thread)
+
+	for thread in threads:
+		thread.join()
+
+except Exception as ex:
+	print("Unable to use Threads")
+	print(ex)
+	exit(1)
+
+linksTuples = []
+for key in linkCount:
+    linksTuples.append((linkCount[key], key))
+
+linksTuples.sort()
+for urlTuple in linksTuples:
+    print(urlTuple)
